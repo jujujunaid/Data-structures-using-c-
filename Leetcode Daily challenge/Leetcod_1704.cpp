3760fd46a4f99b3498f95cbedc5db100f7c39be6
Junaid <mohammadjunaid44@gmail.com>
2024-01-12 21:18:29 +0530

    Create Leetcod_1704.cpp

---
+++ Leetcode Daily challenge/Leetcod_1704.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+       int mid =s.length()/2;
+        int c1=0,c2=0;
+        for(int i=0;i<mid;i++)
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
+            c1++;
+     for(int i=mid;i<s.length();i++)
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
+            c2++;
+        if(c1==c2)
+        return 1;
+        return 0;
+    }
+};
