fc7b66e2b9193989603cf11c57151c8197c04136
Junaid <mohammadjunaid44@gmail.com>
2024-01-25 22:27:15 +0530

    Create Leetcode_1143.cpp

---
+++ Leetcode Daily challenge/Leetcode_1143.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solven(string& a,string&b){
+       vector<vector<int> >v(a.length()+1,vector<int>(b.length()+1,0));
+       for(int i=a.length()-1;i>=0;i--){
+           for(int j=b.length()-1;j>=0;j--){
+               int ans=0;
+            if(a[i]==b[j]){
+          ans=1+v[i+1][j+1];
+      }
+      else{
+          ans=max(v[i+1][j],v[i][j+1]);
+      }
+      v[i][j]=ans;
+           }
+       }
+       return v[0][0]; 
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+     return solven(text1,text2);  
+    }
+};
